682b587be019f0773ad063cc64156fc5399d96a8
Y-admen <yassminash305@gmail.com>
2024-09-09 10:43:30 +0300

    semistandard validation

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,26 +6,26 @@ const filmId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        const filmData = JSON.parse(body);
-        const characters = filmData.characters;
+  if (error) {
+    console.error(error);
+  } else {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
 
-        const fetchCharacter = (index) => {
-            if (index < characters.length) {
-                request(characters[index], (error, response, body) => {
-                    if (error) {
-                        console.error(error);
-                    } else {
-                        const characterData = JSON.parse(body);
-                        console.log(characterData.name);
-                        fetchCharacter(index + 1);
-                    }
-                });
-            }
-        };
+    const fetchCharacter = (index) => {
+      if (index < characters.length) {
+        request(characters[index], (error, response, body) => {
+          if (error) {
+            console.error(error);
+          } else {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+            fetchCharacter(index + 1);
+          }
+        });
+      }
+    };
 
-        fetchCharacter(0);
-    }
+    fetchCharacter(0);
+  }
 });
