253c3e9ed45d97cd1f218d83ae36951cdd8f02c2
Y-admen <yassminash305@gmail.com>
2024-09-19 23:36:01 +0300

    making_change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module for the makeChange function.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a target amount 'total'
+    Return min num of coins
+    """
+    if total <= 0:
+        return 0
+
+
+    # Initialize dp arr with large value to compare with min
+    dp = [float('inf')] * (total + 1) # (total + 1) to include base case
+    # Base case: 0 coins needed to make amount 0
+    dp[0] = 0
+
+    # Iterate over each coin
+    for coin in coins:
+        for x in range(coin, total + 1):
+            # Update the dp array for each amount from coin to total
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+
+    # If dp[total] still float('inf'),means that total cannot be met
+    # by any number of coins
+    return dp[total] if dp[total] != float('inf') else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,10 @@
+# Coin Change Problem
+
+This project contains a function to determine the fewest number of coins needed to meet a given amount total.
+
+## Function
+
+- `makeChange(coins, total)`
+
+visualized solution
+![alt text](<Screenshot (2314).png>)
